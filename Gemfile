64c3ac8e9ca80d8b038bd007c693f6f99d2415ff
jphager2 <jphager2@gmail.com>
2016-11-07 14:34:43 +0100

    publish zine to manga download dir

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'rpub'
+source 'https://rubygems.org'
+
+gem 'mangdown', '~> 0.13'
+gem 'pg'
+gem 'activerecord', '~> 4.0'
--- lib/my_manga_zine.rb
+++ lib/my_manga_zine.rb
@@ -1,13 +1,25 @@
 require 'mangdown'
 require_relative '../../my_manga/db/environment'
+require_relative '../../my_manga/lib/my_manga'
 
 module MyManga
   module Zine
-    def self.publish
+    def self.publish(name)
+      Dir.mkdir('tmp') unless Dir.exist?('tmp')
+
       zine = zine_content
+      serialized_name = []
       zine.each do |chapter|
-        chapter.to_md.download_to(Dir.pwd + '/zine')
+        serialized_name << chapter.id
+        chapter.to_md.download_to(Dir.pwd + '/tmp')
+        MyManga.read!(chapter.manga, [chapter.number]) if read_on_publish?
       end
+
+      serialized_name = serialized_name.join.to_i.to_s(32)
+
+      cbz("#{name}-#{serialized_name}")
+
+      utils.rm_r('tmp') if clean_up_files?
 
       # Create an epub from the files (use rpub)
       # Need to get the styles right
@@ -34,6 +46,37 @@ module MyManga
         zine.first(chapter_count).sort_by do |chapter|
           [chapter.manga_id, chapter.number]
         end
+      end
+
+      def cbz(dir)
+        pages = Dir['tmp/**/*.*']
+
+        Dir.mkdir(dir) unless Dir.exist?(dir)
+
+        pages.each do |page|
+          filename = File.basename(page)
+          utils.cp(page, File.join(MyManga.download_dir, dir, filename))
+        end
+
+        Mangdown::CBZ.one(dir)
+
+        utils.rm_r(dir) if clean_up_files?
+      end
+
+      def read_on_publish?
+        ENV['MY_MANGA_ZINE_NO_READ'].nil? && !debug?
+      end
+
+      def clean_up_files?
+        ENV['MY_MANGA_ZINE_CLEAN_UP'].nil? && !debug?
+      end
+
+      def debug?
+        ENV['DEBUG'] || $DEBUG
+      end
+
+      def utils
+        debug? ? FileUtils::Verbose : FileUtils
       end
     end
   end
